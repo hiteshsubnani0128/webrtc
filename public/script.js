28fbd9c6ef0ec6b0fd76d91b1133d09ac68b6798
Hitesh Subnani <hiteshsubnani734@gmail.com>
2020-07-27 13:04:24 +0530

    I added peer js

--- public/script.js
+++ public/script.js
@@ -1,9 +1,6 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
-const myPeer = new Peer(undefined, {
-  host: '/',
-  port: '3001'
-})
+const myPeer = new Peer({host:'peerjs-server.herokuapp.com', secure:true, port:443})
 
 
 let muted = false;

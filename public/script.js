d854905a3f3c5c4f56405be0a72d20d478603643
Hitesh Subnani <hiteshsubnani734@gmail.com>
2020-07-31 23:24:20 +0530

    I added some code

--- public/script.js
+++ public/script.js
@@ -52,6 +52,7 @@ navigator.mediaDevices.getUserMedia({
 
       call.on('stream', userVideoStream => {
         addVideoStream(video, userVideoStream)
+        console.log("ASD", userVideoStream);
         ovideo = video;
       })
     })
@@ -166,3 +167,44 @@ function sendChat() {
   }
   console.log();
 }
+
+var canvas = document.getElementById('updating-chart'),
+    ctx = canvas.getContext('2d'),
+    startingData = {
+      labels: [0, 2, 4, 6, 8, 10, 12],
+      datasets: [
+          {   
+              label: 'user1',
+              fillColor: "rgba(220,220,220,0.2)",
+              strokeColor: "rgba(220,220,220,1)",
+              pointColor: "rgba(220,220,220,1)",
+              pointStrokeColor: "#fff",
+              data: [65, 59, 80, 81, 56, 55, 40]
+          },
+          {
+              label: userName,
+              fillColor: "rgba(151,187,205,0.2)",
+              strokeColor: "rgba(151,187,205,1)",
+              pointColor: "rgba(151,187,205,1)",
+              pointStrokeColor: "#fff",
+              data: [28, 48, 40, 19, 86, 27, 90]
+          }
+      ]
+    };
+
+// Reduce the animation steps for demo clarity.
+var myLiveChart = new Chart(ctx,{
+  type: 'line',
+  data: startingData
+});
+
+
+setInterval(function(){
+  // Get a random index point
+  var indexToUpdate = Math.round(Math.random() * startingData.labels.length);
+  
+  // Update one of the points in the second dataset
+  myLiveChart.data.datasets[0].data[indexToUpdate] = Math.random() * 100;
+  
+  myLiveChart.update();
+}, 500);
